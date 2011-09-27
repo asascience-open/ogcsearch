21dd049934af345978e0d82b6150177b814355b2
Kyle Wilcox <kwilcox@asascience.com>
2011-09-27 17:57:04 -0400

    Move populate to the admin controller, improve routes, add inflection

--- app/controllers/admin/wms_controller.rb
+++ app/controllers/admin/wms_controller.rb
@@ -4,6 +4,38 @@ class Admin::WmsController < Admin::AdminController
 
   end
 
+  def populate
+    columns = params[:sColumns].split(",")
+    sort_direction = params[:sSortDir_0]
+    sort_column = columns[params[:iSortingCols].to_i]
+    page_num = (params[:iDisplayStart].to_i / params[:iDisplayLength].to_i) + 1
+    if params[:sSearch].blank?
+      servers = WmsServer.order_by(sort_column, sort_direction).page(page_num.to_i).per(params[:iDisplayLength].to_i)
+    else
+      servers = WmsServer.fulltext_search(params[:sSearch]).sort do |x,y|
+        if sort_direction == "asc"
+          y[sort_column.to_sym] <=> x[sort_column.to_sym]
+        else
+          x[sort_column.to_sym] <=> y[sort_column.to_sym]
+        end
+      end
+      servers = Kaminari.paginate_array(servers).page(page_num).per(params[:iDisplayLength].to_i)
+    end
+
+    respond_to do |format|
+      format.dataTable {
+        render :json => {
+        :sEcho => params[:sEcho],
+        :iTotalRecords => servers.total_count,
+        :iTotalDisplayRecords => servers.total_count,
+        :aaData => servers.as_json(
+          :only => [:name, :title, :url, :keywords, :tags, :scanned]
+        )}
+      }
+      format.html
+    end
+  end
+
   def destroy
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Ogcsearch::Application.routes.draw do
 
   namespace :admin do
-    resources :wms
+    resources :wms, :only => [:index, :destroy] do
+      get 'populate', :on => :collection
+    end
   end
 
   match 'wms/find.:format' => 'wms#find'
   match 'wms/parse' => 'wms#parse'
   match 'wms/status.:format' => 'wms#status'
   match 'wms/search.:format' => 'wms#search'
-  match 'wms/index.:format' => 'wms#index'
-  match 'wms/populate.:format' => 'wms#populate'
 
   root :to => "client#index"
 
