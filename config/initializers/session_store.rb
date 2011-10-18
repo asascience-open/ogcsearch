8944bd5fb0f112475e9a4eee6d08706e8c621532
Kyle Wilcox <kwilcox@asascience.com>
2011-10-18 13:44:52 -0400

    Fix the session key (kills active sessions/remembers!)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-OgcSearch::Application.config.session_store :cookie_store, key: '_ogc-search-3.1_session'
+OgcSearch::Application.config.session_store :cookie_store, key: '_ogc-search_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
