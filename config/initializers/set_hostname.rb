95769d0799fa807698290661bb5178807bf1a1c2
Kyle Wilcox <kwilcox@asascience.com>
2011-10-18 09:58:27 -0400

    HOSTNAME should have an http:// before it

--- config/initializers/set_hostname.rb
+++ config/initializers/set_hostname.rb
@@ -1,1 +1,1 @@
-HOSTNAME="#{ActionMailer::Base.default_url_options[:host]}:#{ActionMailer::Base.default_url_options[:port].to_s}" rescue nil
+HOSTNAME="http://#{ActionMailer::Base.default_url_options[:host]}:#{ActionMailer::Base.default_url_options[:port].to_s}" rescue nil
