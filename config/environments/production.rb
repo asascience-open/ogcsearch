d7a9bbe2d75d137fb32b09471dea60545f87983d
Kyle Wilcox <kwilcox@asascience.com>
2011-10-18 13:44:32 -0400

    Fix some asset paths

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,8 @@ OgcSearch::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+  config.assets.initialize_on_precompile = false
+
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
