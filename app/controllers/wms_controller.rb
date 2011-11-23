62cdd2f9160bdb541ef707520c8fdd560039057e
Kyle Wilcox <kwilcox@asascience.com>
2011-11-23 15:50:20 -0500

    String search for finding getcapabilities, not just embedded href links

--- app/controllers/wms_controller.rb
+++ app/controllers/wms_controller.rb
@@ -68,12 +68,29 @@ class WmsController < ApplicationController
   end
 
   def extract
-    doc = Nokogiri::HTML(open(@fixed_url))
-    hrefs = doc.xpath("//a/@href").map do |s|
-      if /service=wms/i =~ s.text && /request=getcapabilities/i =~ s.text
-        URI::join(@fixed_url,s.text).to_s.gsub(/([^:])\/\//, '\1/') rescue nil
+    file = open(@fixed_url)
+    r = file.read
+    
+    scan_hrefs = f.scan(/[a-zA-Z0-9\&=?\.\/:]+request=getcapabilities[a-zA-Z0-9\&=?]*(?:\.[0-9])*/i).map do |k|
+      if /service=wms/i =~ k
+        # Normalize into a URI. This handles relative links (if needed)
+        URI::join(@fixed_url,k).to_s.gsub(/([^:])\/\//, '\1/') rescue nil
       end
-    end.compact.uniq
+    end.compact
+
+    link_hrefs = []
+
+    # This isn't actually pulling out any additional information, is it?!?!
+    #doc = Nokogiri::HTML(r)
+    #link_hrefs = doc.xpath("//a/@href").map do |s|
+    #  if /service=wms/i =~ s.text && /request=getcapabilities/i =~ s.text
+    #    # Normalize into a URI. This handles relative links!
+    #    URI::join(@fixed_url,s.text).to_s.gsub(/([^:])\/\//, '\1/') rescue nil
+    #  end
+    #end.compact
+
+    hrefs = (link_hrefs + scan_hrefs).uniq
+
     respond_to do |format|
       format.json { render :json => hrefs }
     end
